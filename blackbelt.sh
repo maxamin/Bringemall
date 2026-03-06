865b2283a02dd58c556b18595ef9f0812a23a272
Mohamed Amine <playgesmii@gmail.com>
2026-03-05 23:17:58 -0500

    Refactor blackbelt.sh for new paths and optimizations
    Updated script parameters and added new tools for improved functionality.

--- blackbelt.sh
+++ blackbelt.sh
@@ -4,46 +4,37 @@ set -e
 set -o pipefail
 
 INPUT=$1
-THREADS=800
+THREADS=700
 
-BASE=$HOME/godmode_recon
+BASE=$HOME/godmode_v2
 TOOLS=$BASE/tools
 DATA=$BASE/data
 RESOLVERS=$BASE/resolvers.txt
+WORDLIST=$BASE/subdomains.txt
 
 mkdir -p $TOOLS
 mkdir -p $DATA
 
-############################################
-# SYSTEM OPTIMIZATION
-############################################
-
 ulimit -n 1000000
 export GOMAXPROCS=$(nproc)
+export GOMEMLIMIT=8GiB
 
 sysctl -w net.core.somaxconn=65535
 sysctl -w net.ipv4.tcp_max_syn_backlog=65535
 sysctl -w net.ipv4.ip_local_port_range="1024 65535"
-
-############################################
-# INSTALL DEPENDENCIES
-############################################
+sysctl -w net.ipv4.tcp_tw_reuse=1
+sysctl -w net.ipv4.tcp_fin_timeout=15
 
 apt update
 
 apt install -y \
-git curl wget jq tmux parallel \
-dnsutils build-essential unzip \
-proxychains4 python3 python3-pip \
+git curl wget jq tmux parallel dnsutils \
+build-essential unzip proxychains4 python3 python3-pip \
 libpcap-dev golang-go
 
 mkdir -p $HOME/go/bin
 export GOPATH=$HOME/go
 export PATH=$PATH:$HOME/go/bin
-
-############################################
-# INSTALL GO TOOLS
-############################################
 
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/tomnomnom/assetfinder@latest
@@ -62,32 +53,26 @@ go install github.com/tomnomnom/anew@latest
 go install github.com/sensepost/gowitness@latest
 go install github.com/projectdiscovery/uncover/cmd/uncover@latest
 go install github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
-
-############################################
-# NUCLEI TEMPLATES
-############################################
+go install github.com/d3mondev/puredns/v2@latest
+go install github.com/projectdiscovery/subzy/cmd/subzy@latest
 
 nuclei -update-templates
-
-############################################
-# WORDLISTS
-############################################
 
 mkdir -p /usr/share/wordlists/dirbuster
 
 wget -q https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-medium.txt \
 -O /usr/share/wordlists/dirbuster/dir.txt
 
-############################################
-# RESOLVERS
-############################################
-
 wget -q https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt \
 -O $RESOLVERS
 
-############################################
-# RECON LOOP
-############################################
+wget -q https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-top1million-5000.txt \
+-O $WORDLIST
+
+git clone https://github.com/blechschmidt/massdns.git $TOOLS/massdns
+cd $TOOLS/massdns
+make
+cd
 
 while true
 do
@@ -95,13 +80,9 @@ do
 RUN=$(date +%s)
 OUT=$DATA/run_$RUN
 
-mkdir -p $OUT/{subs,resolved,live,ports,urls,js,secrets,params,fuzz,xss,nuclei}
+mkdir -p $OUT/{subs,resolved,live,ports,urls,js,secrets,params,fuzz,xss,nuclei,takeover}
 
-############################################
-# SUBDOMAIN ENUMERATION
-############################################
-
-cat $INPUT | parallel -j 150 '
+cat $INPUT | parallel -j 120 '
 
 subfinder -d {} -silent
 assetfinder --subs-only {}
@@ -109,28 +90,31 @@ chaos -d {}
 amass enum -passive -d {}
 
 curl -s https://crt.sh/\?q=%25.{}\\&output=json \
-| jq -r ".[].name_value" \
-| sed "s/\*\.//g"
+| jq -r ".[].name_value" | sed "s/\*\.//g"
 
 curl -s https://rapiddns.io/subdomain/{}?full=1 \
 | grep -oP "(?<=<td>)[^<]+" | grep {}
 
-' | anew $OUT/subs/all_subdomains.txt
+' | anew $OUT/subs/passive.txt
 
-############################################
-# DNS RESOLUTION
-############################################
+cat $INPUT | parallel -j 80 '
 
-dnsx \
--l $OUT/subs/all_subdomains.txt \
+puredns bruteforce '"$WORDLIST"' {} \
+-r '"$RESOLVERS"' \
+-w -
+
+' | anew $OUT/subs/bruteforce.txt
+
+cat $OUT/subs/passive.txt $OUT/subs/bruteforce.txt | sort -u > $OUT/subs/all.txt
+
+$TOOLS/massdns/bin/massdns \
 -r $RESOLVERS \
--silent \
--threads $THREADS \
--o $OUT/resolved/resolved.txt
+-t A \
+-o S \
+-w $OUT/subs/massdns.txt \
+$OUT/subs/all.txt
 
-############################################
-# HTTP PROBING
-############################################
+cut -d" " -f1 $OUT/subs/massdns.txt | sed "s/\.$//" | sort -u > $OUT/resolved/resolved.txt
 
 httpx \
 -l $OUT/resolved/resolved.txt \
@@ -140,112 +124,70 @@ httpx \
 -ip \
 -server \
 -cdn \
--random-agent \
 -follow-redirects \
+-random-agent \
 -silent \
 -threads $THREADS \
--o $OUT/live/live_hosts.txt
-
-############################################
-# PORT SCANNING
-############################################
+-o $OUT/live/live.txt
 
 naabu \
 -l $OUT/resolved/resolved.txt \
 -top-ports 1000 \
 -rate 20000 \
--o $OUT/ports/open_ports.txt
+-o $OUT/ports/ports.txt
 
-############################################
-# URL COLLECTION
-############################################
+subzy run \
+--targets $OUT/resolved/resolved.txt \
+--hide_fails \
+--output $OUT/takeover/takeovers.txt
 
-cat $OUT/live/live_hosts.txt | gau >> $OUT/urls/urls_raw.txt
-cat $OUT/live/live_hosts.txt | waybackurls >> $OUT/urls/urls_raw.txt
-
-############################################
-# CRAWLING
-############################################
+cat $OUT/live/live.txt | gau >> $OUT/urls/raw.txt
+cat $OUT/live/live.txt | waybackurls >> $OUT/urls/raw.txt
 
 katana \
--l $OUT/live/live_hosts.txt \
+-l $OUT/live/live.txt \
 -depth 5 \
 -js-crawl \
 -jsluice \
 -automatic-form-fill \
 -silent \
--o $OUT/urls/crawled.txt
+-o $OUT/urls/crawl.txt
 
-cat $OUT/urls/urls_raw.txt $OUT/urls/crawled.txt \
-| sort -u > $OUT/urls/all_urls.txt
+cat $OUT/urls/raw.txt $OUT/urls/crawl.txt | sort -u > $OUT/urls/all.txt
 
-############################################
-# PARAMETER DISCOVERY
-############################################
+grep "=" $OUT/urls/all.txt | sort -u > $OUT/params/params.txt
 
-grep "=" $OUT/urls/all_urls.txt \
-| sort -u > $OUT/params/params.txt
+grep "\.js" $OUT/urls/all.txt | sort -u > $OUT/js/js.txt
 
-############################################
-# JS FILE EXTRACTION
-############################################
+cat $OUT/js/js.txt | parallel -j 100 '
 
-grep "\.js" $OUT/urls/all_urls.txt \
-| sort -u > $OUT/js/js_files.txt
+curl -s {} | grep -Ei "(apikey|token|secret|password|client_secret|access_key|aws_access_key_id|aws_secret_access_key)"
 
-############################################
-# JS SECRET SCANNING
-############################################
+' >> $OUT/secrets/js.txt
 
-cat $OUT/js/js_files.txt | parallel -j 100 '
+cat $OUT/live/live.txt | parallel -j 60 '
 
-curl -s {} | grep -E \
-"apikey|token|secret|password|auth|client_secret|access_key|aws_access_key_id|aws_secret_access_key"
-
-' >> $OUT/secrets/js_secrets.txt
-
-############################################
-# DIRECTORY FUZZING
-############################################
-
-cat $OUT/live/live_hosts.txt | parallel -j 80 '
-
-ffuf -u {}/FUZZ \
+ffuf \
+-u {}/FUZZ \
 -w /usr/share/wordlists/dirbuster/dir.txt \
--t 80 \
+-t 60 \
 -mc all \
 -fs 0 \
 -s
 
 ' >> $OUT/fuzz/ffuf.txt
 
-############################################
-# XSS SCANNING
-############################################
-
-cat $OUT/params/params.txt | dalfox pipe --silence \
->> $OUT/xss/xss.txt
-
-############################################
-# NUCLEI SCANNING
-############################################
+cat $OUT/params/params.txt | dalfox pipe --silence >> $OUT/xss/xss.txt
 
 nuclei \
--l $OUT/live/live_hosts.txt \
--c 800 \
--rate-limit 4000 \
+-l $OUT/live/live.txt \
+-c 700 \
+-rate-limit 3500 \
 -o $OUT/nuclei/nuclei.txt
 
-############################################
-# SCREENSHOTS
-############################################
+gowitness scan file -f $OUT/live/live.txt
 
-gowitness scan file \
--f $OUT/live/live_hosts.txt
-
-############################################
-# RECON DELAY
-############################################
+find $DATA -type d -mtime +7 -exec rm -rf {} +
 
 sleep 86400
 

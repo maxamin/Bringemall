d711ba981cb7553b0d130806a2539edfeefc6ed1
Mohamed Amine <playgesmii@gmail.com>
2026-03-05 23:12:58 -0500

    Enhance blackbelt.sh for better performance and tools
    Updated script to optimize system settings, increased thread count, and changed base directory. Improved dependency installation and added additional tools for reconnaissance.

--- blackbelt.sh
+++ blackbelt.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 INPUT=$1
-THREADS=700
-BASE=$HOME/recon_vps
+THREADS=800
+
+BASE=$HOME/godmode_recon
 TOOLS=$BASE/tools
 DATA=$BASE/data
+RESOLVERS=$BASE/resolvers.txt
 
 mkdir -p $TOOLS
 mkdir -p $DATA
 
+############################################
+# SYSTEM OPTIMIZATION
+############################################
+
+ulimit -n 1000000
+export GOMAXPROCS=$(nproc)
+
+sysctl -w net.core.somaxconn=65535
+sysctl -w net.ipv4.tcp_max_syn_backlog=65535
+sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+
+############################################
+# INSTALL DEPENDENCIES
+############################################
+
 apt update
-apt install -y git curl wget tmux proxychains4 build-essential unzip jq python3 python3-pip dnsutils libpcap-dev golang-go
+
+apt install -y \
+git curl wget jq tmux parallel \
+dnsutils build-essential unzip \
+proxychains4 python3 python3-pip \
+libpcap-dev golang-go
 
 mkdir -p $HOME/go/bin
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc
-source ~/.bashrc
+export GOPATH=$HOME/go
+export PATH=$PATH:$HOME/go/bin
+
+############################################
+# INSTALL GO TOOLS
+############################################
 
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/tomnomnom/assetfinder@latest
@@ -30,58 +58,194 @@ go install github.com/hahwul/dalfox/v2@latest
 go install github.com/ffuf/ffuf/v2@latest
 go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+go install github.com/tomnomnom/anew@latest
+go install github.com/sensepost/gowitness@latest
+go install github.com/projectdiscovery/uncover/cmd/uncover@latest
+go install github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
+
+############################################
+# NUCLEI TEMPLATES
+############################################
 
 nuclei -update-templates
 
+############################################
+# WORDLISTS
+############################################
+
 mkdir -p /usr/share/wordlists/dirbuster
-wget -q https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-medium.txt -O /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
 
-ulimit -n 1000000
-export GOMAXPROCS=$(nproc)
+wget -q https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-medium.txt \
+-O /usr/share/wordlists/dirbuster/dir.txt
 
-sysctl -w net.core.somaxconn=65535
-sysctl -w net.ipv4.tcp_max_syn_backlog=65535
-sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+############################################
+# RESOLVERS
+############################################
+
+wget -q https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt \
+-O $RESOLVERS
+
+############################################
+# RECON LOOP
+############################################
 
 while true
 do
 
-mkdir -p $DATA/run_$(date +%s)
-OUT=$DATA/run_$(date +%s)
+RUN=$(date +%s)
+OUT=$DATA/run_$RUN
 
-cat $INPUT | xargs -P 200 -I{} sh -c '
-proxychains4 subfinder -d {} -silent
-proxychains4 assetfinder --subs-only {}
-proxychains4 chaos -d {}
-proxychains4 amass enum -passive -d {}
-' >> $OUT/subs_raw.txt
+mkdir -p $OUT/{subs,resolved,live,ports,urls,js,secrets,params,fuzz,xss,nuclei}
 
-sort -u $OUT/subs_raw.txt > $OUT/subdomains.txt
+############################################
+# SUBDOMAIN ENUMERATION
+############################################
 
-proxychains4 dnsx -l $OUT/subdomains.txt -silent -threads $THREADS -o $OUT/resolved.txt
+cat $INPUT | parallel -j 150 '
 
-proxychains4 naabu -l $OUT/resolved.txt -top-ports 1000 -rate 15000 -o $OUT/open_ports.txt
+subfinder -d {} -silent
+assetfinder --subs-only {}
+chaos -d {}
+amass enum -passive -d {}
 
-proxychains4 httpx -l $OUT/resolved.txt -silent -threads $THREADS -title -tech-detect -status-code -ip -cdn -server -o $OUT/live_hosts.txt
+curl -s https://crt.sh/\?q=%25.{}\\&output=json \
+| jq -r ".[].name_value" \
+| sed "s/\*\.//g"
 
-cat $OUT/live_hosts.txt | gau >> $OUT/urls_raw.txt
-cat $OUT/live_hosts.txt | waybackurls >> $OUT/urls_raw.txt
+curl -s https://rapiddns.io/subdomain/{}?full=1 \
+| grep -oP "(?<=<td>)[^<]+" | grep {}
 
-proxychains4 katana -l $OUT/live_hosts.txt -depth 5 -js-crawl -jsluice -automatic-form-fill -silent -o $OUT/crawled_urls.txt
+' | anew $OUT/subs/all_subdomains.txt
 
-cat $OUT/urls_raw.txt $OUT/crawled_urls.txt | sort -u > $OUT/all_urls.txt
+############################################
+# DNS RESOLUTION
+############################################
 
-grep "=" $OUT/all_urls.txt | sort -u > $OUT/params.txt
+dnsx \
+-l $OUT/subs/all_subdomains.txt \
+-r $RESOLVERS \
+-silent \
+-threads $THREADS \
+-o $OUT/resolved/resolved.txt
 
-grep ".js" $OUT/all_urls.txt | sort -u > $OUT/js_files.txt
+############################################
+# HTTP PROBING
+############################################
 
-cat $OUT/js_files.txt | xargs -P 150 -I{} curl -s {} | grep -E "apikey|token|secret|password|auth|client_secret|access_key|aws_access_key_id|aws_secret_access_key" >> $OUT/js_secrets.txt
+httpx \
+-l $OUT/resolved/resolved.txt \
+-title \
+-status-code \
+-tech-detect \
+-ip \
+-server \
+-cdn \
+-random-agent \
+-follow-redirects \
+-silent \
+-threads $THREADS \
+-o $OUT/live/live_hosts.txt
 
-cat $OUT/live_hosts.txt | xargs -P 120 -I{} proxychains4 ffuf -u {}/FUZZ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 120 -mc all -fs 0 -s >> $OUT/ffuf_results.txt
+############################################
+# PORT SCANNING
+############################################
 
-cat $OUT/params.txt | dalfox pipe --silence >> $OUT/xss_results.txt
+naabu \
+-l $OUT/resolved/resolved.txt \
+-top-ports 1000 \
+-rate 20000 \
+-o $OUT/ports/open_ports.txt
 
-proxychains4 nuclei -l $OUT/live_hosts.txt -t ~/nuclei-templates -c 700 -rate-limit 2500 -o $OUT/nuclei_results.txt
+############################################
+# URL COLLECTION
+############################################
+
+cat $OUT/live/live_hosts.txt | gau >> $OUT/urls/urls_raw.txt
+cat $OUT/live/live_hosts.txt | waybackurls >> $OUT/urls/urls_raw.txt
+
+############################################
+# CRAWLING
+############################################
+
+katana \
+-l $OUT/live/live_hosts.txt \
+-depth 5 \
+-js-crawl \
+-jsluice \
+-automatic-form-fill \
+-silent \
+-o $OUT/urls/crawled.txt
+
+cat $OUT/urls/urls_raw.txt $OUT/urls/crawled.txt \
+| sort -u > $OUT/urls/all_urls.txt
+
+############################################
+# PARAMETER DISCOVERY
+############################################
+
+grep "=" $OUT/urls/all_urls.txt \
+| sort -u > $OUT/params/params.txt
+
+############################################
+# JS FILE EXTRACTION
+############################################
+
+grep "\.js" $OUT/urls/all_urls.txt \
+| sort -u > $OUT/js/js_files.txt
+
+############################################
+# JS SECRET SCANNING
+############################################
+
+cat $OUT/js/js_files.txt | parallel -j 100 '
+
+curl -s {} | grep -E \
+"apikey|token|secret|password|auth|client_secret|access_key|aws_access_key_id|aws_secret_access_key"
+
+' >> $OUT/secrets/js_secrets.txt
+
+############################################
+# DIRECTORY FUZZING
+############################################
+
+cat $OUT/live/live_hosts.txt | parallel -j 80 '
+
+ffuf -u {}/FUZZ \
+-w /usr/share/wordlists/dirbuster/dir.txt \
+-t 80 \
+-mc all \
+-fs 0 \
+-s
+
+' >> $OUT/fuzz/ffuf.txt
+
+############################################
+# XSS SCANNING
+############################################
+
+cat $OUT/params/params.txt | dalfox pipe --silence \
+>> $OUT/xss/xss.txt
+
+############################################
+# NUCLEI SCANNING
+############################################
+
+nuclei \
+-l $OUT/live/live_hosts.txt \
+-c 800 \
+-rate-limit 4000 \
+-o $OUT/nuclei/nuclei.txt
+
+############################################
+# SCREENSHOTS
+############################################
+
+gowitness scan file \
+-f $OUT/live/live_hosts.txt
+
+############################################
+# RECON DELAY
+############################################
 
 sleep 86400
 
